fdaea102b7eb10c26961def6f1d6bd2b879db3c6
Nick Beattie <nbeattie@Beattie-MacBook-Pro.local>
2015-09-17 17:04:36 -0400

    register keyboard shortcut

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 var menubar = require('menubar')
 var clipboard = require('clipboard')
+var globalShortcut = require('global-shortcut')
 
 var mb = menubar({
   width: 200,
   height: 100
 })
 
+mb.on('ready', function ready(){
+  var ret = globalShortcut.register('ctrl+alt+\\', function() {
+    console.log('ctrl+alt+\\ is pressed');
+  })
+
+  if (!ret) {
+    console.log('registration failed');
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('ctrl+alt+\\'));
+})
+
+// When menubar opens
 mb.on('show', function ready () {
   clipboard.writeText('¯\\_(ツ)_/¯');
+})
+
+// When menubar application closes
+mb.on('will-quit', function ready(){
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll();
 })
