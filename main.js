3a354d79e039b4530b729ccd73e6f6940f7399d9
Nick Beattie <nbeattie@Beattie-MacBook-Pro.local>
2015-09-17 20:26:48 -0400

    Add system keybinding

--- main.js
+++ main.js
@@ -4,29 +4,32 @@ var globalShortcut = require('global-shortcut')
 
 var mb = menubar({
   width: 200,
-  height: 100
+  height: 100,
+  preloadWindow: true
 })
 
 mb.on('ready', function ready(){
   var ret = globalShortcut.register('ctrl+alt+\\', function() {
-    console.log('ctrl+alt+\\ is pressed');
+    console.log('ctrl+alt+\\ is pressed')
+    // Emit show event
+    mb.showWindow()
   })
 
   if (!ret) {
-    console.log('registration failed');
+    console.log('registration failed')
   }
 
   // Check whether a shortcut is registered.
-  console.log(globalShortcut.isRegistered('ctrl+alt+\\'));
+  console.log(globalShortcut.isRegistered('ctrl+alt+\\') + 'ok')
 })
 
 // When menubar opens
 mb.on('show', function ready () {
-  clipboard.writeText('¯\\_(ツ)_/¯');
+  clipboard.writeText('¯\\_(ツ)_/¯')
 })
 
 // When menubar application closes
 mb.on('will-quit', function ready(){
   // Unregister all shortcuts.
-  globalShortcut.unregisterAll();
+  globalShortcut.unregisterAll()
 })
